8a322b770cdb38a1944d0566cf2cf25487d9d96c
Lachheb Ismaël <isma@localhost.localdomain>
2018-04-14 15:21:21 +0200

    debut de sparql

--- Site/app/App.php
+++ Site/app/App.php
@@ -8,11 +8,14 @@ On peut citer les informations relatives à la bdd ou encore le nom du site
 
 class App{
 
+
 	//Variables statiques représentant la base de données à laquelle on se connecte, ainsi que le nom de notre site
+	private static $database;
 	private static $title = 'Projet Emploi';
 
 	private static $graphe_potter = 'http://fr.dbpedia.org/page/Harry_Potter';
-	private static $graphe_potter_film = 'http://fr.dbpedia.org/page/Harry_Potter_(films)'
+	private static $graphe_potter_film = 'http://fr.dbpedia.org/page/Harry_Potter_(films)';
+
 
 	//Si l'utilisateur aboutit à une page non existante, par exemple en jouant avec l'URL, on renvoie une erreur 404
 	public static function notFound(){
